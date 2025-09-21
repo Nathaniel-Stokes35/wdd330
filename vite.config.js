974da3036880a5dc9cdb0bb226fc4bc0719468c3
Nathaniel-Stokes <Nathaniel.Stokes35@Gmail.com>
2025-09-20 19:46:06 -0500

    fixxing vite.config

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,7 @@ export default defineConfig({
         cart: resolve(__dirname, 'src/cart/index.html'),
         checkout: resolve(__dirname, 'src/checkout/index.html'),
         product: resolve(__dirname, 'src/product_pages/index.html'),
+        search_results: resolve(__dirname, 'src/search_results/index.html')
       },
     },
   },
