c6895563fcb0b1375803f72ff7a83a9590ea26a6
MCLELLANINTERNATIONAL <bro24002@byui.edu>
2025-09-25 22:52:23 +0100

    dup coding product-listing

--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,6 @@ export default defineConfig({
         product: resolve(__dirname, 'src/product_pages/index.html'),
         product_listing: resolve(__dirname, 'src/product_listing/index.html'),
         search_results: resolve(__dirname, 'src/search_results/index.html'),
-        product_listing: resolve(__dirname, 'src/product_listing/index.html'),
         register_page: resolve(__dirname, 'src/register_page/index.html'),
       },
     },
